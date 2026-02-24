69438b6ba0de525a896bd79790841afc453d8c48
Davide Zamboni <davide.zamboni1@gmail.com>
2026-02-24 16:53:54 +0100

    aggiunti transactional

--- src/main/java/com/betacom/car/services/interfaces/IBigModelsServices.java
+++ src/main/java/com/betacom/car/services/interfaces/IBigModelsServices.java
@@ -2,11 +2,19 @@ package com.betacom.car.services.interfaces;
 import java.util.List;
 import com.betacom.car.exceptions.VeicoloException;
 
+import jakarta.transaction.Transactional;
+
 public interface IBigModelsServices<IN, OUT, ID, FILT> {
 	
+	@Transactional (rollbackOn = Exception.class)
 	public Integer create(IN req) throws VeicoloException;
+
+	@Transactional (rollbackOn = Exception.class)
 	public void delete(ID id) throws VeicoloException;
+
+	@Transactional (rollbackOn = Exception.class)
 	void update(IN req) throws VeicoloException;
+
 	List<OUT> findAll() throws VeicoloException;
 	OUT findById(ID id) throws VeicoloException;
 	
