1e5bde834b849c42f3cf41eb7f34348a5e30d58d
boiac <boiac@EnricoPC.fritz.box>
2026-02-25 10:26:36 +0100

    builders

--- src/main/java/com/betacom/car/dto/filters/MacchinaFilter.java
+++ src/main/java/com/betacom/car/dto/filters/MacchinaFilter.java
@@ -1,12 +1,16 @@
 package com.betacom.car.dto.filters;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MacchinaFilter extends VeicoloFilter{
 	
 	private Integer cc;
--- src/main/java/com/betacom/car/dto/filters/MotoFilter.java
+++ src/main/java/com/betacom/car/dto/filters/MotoFilter.java
@@ -1,12 +1,16 @@
 package com.betacom.car.dto.filters;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MotoFilter extends VeicoloFilter{
 	
 	private Integer cc;
