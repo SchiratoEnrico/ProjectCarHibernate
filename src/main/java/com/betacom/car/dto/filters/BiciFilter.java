cbda9c7715114b0e5e65ac38412878e3ac1dee21
boiac <boiac@EnricoPC.fritz.box>
2026-02-25 10:22:53 +0100

    builders per filtri

--- src/main/java/com/betacom/car/dto/filters/BiciFilter.java
+++ src/main/java/com/betacom/car/dto/filters/BiciFilter.java
@@ -1,10 +1,12 @@
 package com.betacom.car.dto.filters;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class BiciFilter extends VeicoloFilter{
 	
 
